14db01ec33c521a6887c0c80f9297bea74193ded
António Santos <santostoni10@hotmail.com>
2020-12-17 16:32:19 +0000

    update on gui

--- src/client/gui/RMPane.java
+++ src/client/gui/RMPane.java
@@ -1,4 +1,25 @@
 package client.gui;
 
-public class RMPane {
+import client.logic.ClientObservable;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class RMPane extends BorderPane implements PropertyChangeListener {
+    private final ClientObservable observable;
+
+
+    public RMPane(ClientObservable observable) {
+        this.observable = observable;
+        this.observable.addPropertyChangeListener(this);
+
+        propertyChange(null);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
--- src/client/gui/RMRoot.java
+++ src/client/gui/RMRoot.java
@@ -1,4 +1,31 @@
 package client.gui;
 
-public class RMRoot {
+import client.logic.ClientController;
+import client.logic.ClientObservable;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.VBox;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class RMRoot extends VBox implements PropertyChangeListener {
+
+    private final ClientObservable observable;
+    private final RMPane RMPane;
+
+    public RMRoot(ClientObservable observable) {
+
+        this.observable = observable;
+        this.observable.addPropertyChangeListener(this);
+
+        RMPane = new RMPane(observable);
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
